f290a24ab9e52041dcba2a50dc82e8055bc4281e
Morti <qstevens@hotmail.be>
2021-04-04 18:18:55 +0200

    [UPD] color theme

--- ctf/urls.py
+++ ctf/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('challenges', views.index),
+    path('challenges', views.challenges),
     path('calendar', views.calendar),
 ]
